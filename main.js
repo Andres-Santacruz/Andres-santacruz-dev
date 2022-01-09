6cdedc05b3f5bae9ede11e580c2748843cf73e67
Andres Santacruz <andres_v10@live.com>
2022-01-09 14:06:18 -0500

    [FEAT]: Add funtionality to dark mode

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const $ = (el) => document.querySelector(el);
+const $$ = (el) => document.querySelectorAll(el);
 const ul = $(".nav-header-menu-list");
 ul.addEventListener("click", (e) => {
   const checkbox = $("#check-menu");
@@ -35,4 +36,22 @@ window.addEventListener("scroll", () => {
     header.style.top = "-100px";
   }
   currentScroll = window.scrollY;
+});
+
+const btn_dark = $(".btn-dark");
+
+btn_dark.addEventListener("click", () => {
+  const body = document.body;
+  const h1_header = $(".nav-header-h1");
+  const li = $$(".nav-header-menu-item");
+  const info_h1 = $(".perfil-info-h1");
+  const info_h2 = $(".perfil-info-h2");
+
+  btn_dark.classList.toggle("btn-dark-active");
+  info_h1.classList.toggle("dark-info-h1");
+  info_h2.classList.toggle("dark-info-h2");
+  h1_header.classList.toggle("dark-color");
+  li.forEach((el) => el.classList.toggle("dark-color"));
+  header.classList.toggle("dark-header");
+  body.classList.toggle("dark");
 });
