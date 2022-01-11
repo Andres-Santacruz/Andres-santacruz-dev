6a31d229f0efec0539c08045d6ab0648838f33a7
Andres Santacruz <andres_v10@live.com>
2022-01-11 14:06:37 -0500

    [REFACTOR]: Changes name variables DOM with $

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const $ = (el) => document.querySelector(el);
 const $$ = (el) => document.querySelectorAll(el);
 
-const ul = $(".nav-header-menu-list");
-const body = document.body;
+const $ul = $(".nav-header-menu-list");
+const $body = document.body;
 const h1_header = $(".nav-header-h1");
-const li = $$(".nav-header-menu-item");
+const $li = $$(".nav-header-menu-item");
 const info_h1 = $(".perfil-info-h1");
 const info_h2 = $(".perfil-info-h2");
 const span_blink = $(".perfil-info-span");
 const btn_dark = $(".btn-dark");
-const span = $(".perfil-info-span");
-const header = $(".header");
+const $span = $(".perfil-info-span");
+const $header = $(".header");
 
-ul.addEventListener("click", (e) => {
+$ul.addEventListener("click", (e) => {
   const checkbox = $("#check-menu");
   checkbox.checked = false;
 });
@@ -22,26 +22,26 @@ let i = 0,
   num = 0;
 let work = text[num].split("");
 const timer = setInterval(() => {
-  span.innerHTML += work[i];
+  $span.innerHTML += work[i];
   i++;
   if (i === work.length) {
-    span.innerHTML = "";
+    $span.innerHTML = "";
     i = 0;
     num += 1;
     work = text[num]?.split("");
   }
   if (num === 3) {
     clearInterval(timer);
-    span.innerHTML = text[2];
+    $span.innerHTML = text[2];
   }
 }, 350);
 
 let currentScroll = window.scrollY;
 window.addEventListener("scroll", () => {
   if (currentScroll > window.scrollY) {
-    header.style.top = "0px";
+    $header.style.top = "0px";
   } else {
-    header.style.top = "-100px";
+    $header.style.top = "-100px";
   }
   currentScroll = window.scrollY;
 });
@@ -52,9 +52,9 @@ const toggleDark = () => {
   info_h1.classList.toggle("dark-info-h1");
   info_h2.classList.toggle("dark-info-h2");
   h1_header.classList.toggle("dark-color");
-  li.forEach((el) => el.classList.toggle("dark-color"));
-  header.classList.toggle("dark-header");
-  body.classList.toggle("dark");
+  $li.forEach((el) => el.classList.toggle("dark-color"));
+  $header.classList.toggle("dark-header");
+  $body.classList.toggle("dark");
 };
 const mediaQueryDark = window.matchMedia("(prefers-color-scheme: dark)");
 if (mediaQueryDark.matches) {
